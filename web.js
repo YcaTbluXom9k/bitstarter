35ca790ebefecc639f49fff7030067862dfe8acb
Anton Fateyev <YcaTblu.Xom9k@gmail.com>
2013-07-10 22:31:15 +0700

    Change getting response message form index.html

--- web.js
+++ web.js
@@ -1,9 +1,12 @@
 var express = require('express');
+var fs = require('fs');
+var buffer = require('buffer');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+  buffer = fs.readFileSync('index.html');
+  response.send(buffer.toString());
 });
 
 var port = process.env.PORT || 5000;
