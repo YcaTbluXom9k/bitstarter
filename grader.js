fdf2d279264efa34e96cb59d3924f89f3c59de1a
Anton Fateyev <YcaTblu.Xom9k@gmail.com>
2013-07-11 09:51:09 +0700

    Corrected grader.js for properly use command line options

--- grader.js
+++ grader.js
@@ -1,33 +1,25 @@
 #!/usr/bin/env node
-/**
- * Created with IntelliJ IDEA.
- * User: ycatbluxom9k
- * Date: 10.07.13
- * Time: 23:42
- * To change this template use File | Settings | File Templates.
- */
-
 /*
- Automatically grade files for the presence of specified HTML tags/attributes.
- Uses commander.js and cheerio. Teaches command line application development
- and basic DOM parsing.
+Automatically grade files for the presence of specified HTML tags/attributes.
+Uses commander.js and cheerio. Teaches command line application development
+and basic DOM parsing.
 
- References:
+References:
 
  + cheerio
- - https://github.com/MatthewMueller/cheerio
- - http://encosia.com/cheerio-faster-windows-friendly-alternative-jsdom/
- - http://maxogden.com/scraping-with-node.html
+   - https://github.com/MatthewMueller/cheerio
+   - http://encosia.com/cheerio-faster-windows-friendly-alternative-jsdom/
+   - http://maxogden.com/scraping-with-node.html
 
  + commander.js
- - https://github.com/visionmedia/commander.js
- - http://tjholowaychuk.com/post/9103188408/commander-js-nodejs-command-line-interfaces-made-easy
+   - https://github.com/visionmedia/commander.js
+   - http://tjholowaychuk.com/post/9103188408/commander-js-nodejs-command-line-interfaces-made-easy
 
  + JSON
- - http://en.wikipedia.org/wiki/JSON
- - https://developer.mozilla.org/en-US/docs/JSON
- - https://developer.mozilla.org/en-US/docs/JSON#JSON_in_Firefox_2
- */
+   - http://en.wikipedia.org/wiki/JSON
+   - https://developer.mozilla.org/en-US/docs/JSON
+   - https://developer.mozilla.org/en-US/docs/JSON#JSON_in_Firefox_2
+*/
 
 var fs = require('fs');
 var program = require('commander');
@@ -38,7 +30,7 @@ var CHECKSFILE_DEFAULT = "checks.json";
 var assertFileExists = function(infile) {
     var instr = infile.toString();
     if(!fs.existsSync(instr)) {
-        console.log("%s does not exist. Existing.", instr);
+        console.log("%s does not exist. Exiting.", instr);
         process.exit(1); // http://nodejs.org/api/process.html#process_process_exit_code
     }
     return instr;
@@ -71,8 +63,8 @@ var clone = function(fn) {
 
 if(require.main == module) {
     program
-        .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
         .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+        .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
         .parse(process.argv);
     var checkJson = checkHtmlFile(program.file, program.checks);
     var outJson = JSON.stringify(checkJson, null, 4);
